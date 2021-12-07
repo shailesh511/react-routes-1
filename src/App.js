b0761e69ab52e53b5811a1991e58612b8b049bc8
Shailesh <shaileshtripathi511@gmail.com>
2021-12-07 13:47:17 +0530

    "routing"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react'
-import {BrowserRouter, Route} from 'react-router-dom'
+import { BrowserRouter,
+  Routes,
+  Route,} from 'react-router-dom'
+import NavBar from './Components/Navbar'
+import Contact from './Components/Contact'
+import Login from './Components/Login'
+import About from './Components/About'
+import Services from './Components/Services'
 
 function App() {
   return (
-   <BrowserRouter>
-      Add your code here
-   </BrowserRouter>
+    <>
+     <BrowserRouter>
+        <NavBar/>
+        <div className="container my-3">
+        <Routes>
+          <Route path="/about-us" element={<About/>}>  </Route>
+          <Route path="/contact" element={<Contact/>}> </Route>
+          <Route path="/services" element={<Services/>}> </Route>
+          <Route path="/login" element={<Login/>}> </Route>
+        </Routes>
+        </div>
+        </BrowserRouter>
+   </>
   )
 }
 
